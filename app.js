30bdb20a58e08adf48b5c9c44187beb294d8f61c
jao <jaoprogramador@gmail.com>
2024-12-19 12:33:45 +0100

    NUEVO REST HEALTh

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('dist'));
 
 app.get('/health', (req, res) => {
+  console.log('POKEMON:::health::ok');
   res.send('ok');
 });
 app.listen(PORT, () => {
