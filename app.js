f0e72594b2246e5586cff84aa06f3d338ebd0a32
jao <jaoprogramador@gmail.com>
2024-12-19 12:28:06 +0100

    pipeline deploy render

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 //const PORT = process.env.PORT || 4000;
 app.use(express.static('dist'));
 
+app.get('/health', (req, res) => {
+  res.send('ok');
+});
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
