4283c4847fce2759096d3e52203d7cf05da59c26
jao <jaoprogramador@gmail.com>
2024-12-19 15:18:55 +0100

    ERROR 404

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+// get the port from env variable
+const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 4000;
 app.get('/health', (req, res) => {
   console.log('POKEMON:::health::ok');
   res.send('ok');
@@ -8,9 +12,7 @@ app.get('/hola', (req, res) => {
   console.log('POKEMON:::hola::adios');
   res.send('adios');
 });
-// get the port from env variable
-const PORT = process.env.PORT || 5000;
-//const PORT = process.env.PORT || 4000;
+
 app.use(express.static('dist'));
 
 
