42d9d0ef632a4398451cea0b0d85f01e462dd74c
jao <jaoprogramador@gmail.com>
2024-12-19 11:35:29 +0100

    Error 4000 port

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
-
+//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
